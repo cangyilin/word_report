a906e9b648d3e485f865a2af2bad0b34601eb0eb
Charlin <cangyilin520@gmail.com>
2023-04-17 18:35:52 +0800

    增加一些目录

---
+++ medical_report/config/settings.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Organization  : Novogene
+# @Author        : Charlin(7900)
+# @Time          : 2023/4/17 18:14
+# @Function      :
+
+# 数据库配置
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# 日志配置
+LOGGING_LEVEL = 'DEBUG'
+
+
+# 路径配置
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+# 邮件配置
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'myemail@gmail.com'
+EMAIL_HOST_PASSWORD = 'mypassword'
+
+#
